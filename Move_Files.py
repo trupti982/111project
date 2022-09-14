d5605ee186793a9094f3f24a9413da73b06d5ea4
trupti982 <113361482+trupti982@users.noreply.github.com>
2022-09-14 21:49:33 +0530

    Add files via upload

---
+++ Move_Files.py
@@ -0,0 +1,21 @@
+import os
+import shutil
+
+# .exe , .msi,  .gif, .png .jpg, .jpeg, .csv, .pdf , .xls , .xlsx , .ppt , .pptx
+
+from_dir = "C:/Users/shaur/Downloads/sample"
+to_dir = "C:/Users/shaur/OneDrive/Desktop/Downloaded_files"
+
+list_of_files = os.listdir(from_dir)
+print(list_of_files)
+
+# Move All Image files from Downloads Folder to Another Folder
+for file_name in list_of_files:
+
+    name, extension = os.path.splitext(file_name)
+    #print(name)
+    #print(extension)
+
+   
+
+        
